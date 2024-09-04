ef6d646432a7a567e03d99535fa09068e298ad91
DESKTOP-VF0SR3O\TRpad02 <zara6laura@gmail.com>
2024-09-04 11:03:49 -0600

    Se agrega los proyectos negocio, infraestructura y choferes

---
+++ Gen9-Parent/Gen9-negocio/Gen9-choferes/src/main/java/com/gonzalez/choferes/Gen9ChoferesApplication.java
@@ -0,0 +1,13 @@
+package com.gonzalez.choferes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gen9ChoferesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Gen9ChoferesApplication.class, args);
+	}
+
+}
---
+++ Gen9-Parent/Gen9-negocio/Gen9-choferes/src/test/java/com/gonzalez/choferes/Gen9ChoferesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gonzalez.choferes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gen9ChoferesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
